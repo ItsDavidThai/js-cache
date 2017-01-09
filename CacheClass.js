d30d2268950944a0e7d1f3712f8ef6addc036829
itsdavidthai <itsdavidthai@gmail.com>
2017-01-08 21:57:29 -0800

    add design document, fix tests, and add comments

--- CacheClass.js
+++ CacheClass.js
@@ -7,6 +7,16 @@
 // - should be able to handle multiple levels of caching (ie, be able to look into a larger/parent cache if there is a cache miss)
 
 
+/*
+  Store Constructor
+  - creates a store instance that the jsCache instance can use  
+*/
+function Store(){
+  // store is represented as key:value pairs
+  // the key represents the address
+  this.store = {}
+}
+
 // hashing function to find the index in the initial set
 function cacheSetHash(key, cacheSetSize){
   return key.length % cacheSetSize
@@ -14,6 +24,7 @@ function cacheSetHash(key, cacheSetSize){
 
 /*
   Queue Constructor
+  - Used to handle evictions when a cache set is full
 */
 function Queue() {
   this.queue = []
@@ -33,103 +44,32 @@ Queue.prototype.getState= function () {
 
 
 
-// sets combine mapped and associative properties 
-function jsCache(cacheSetSize, setBlockSize, store, parentCache, childCache) {
+ /*
+  jsCache Constructor
+  - Create cache instances
+  Params:[
+    cacheSetSize: set the size of the cache
+    setBlockSize: set the size of each block in a set
+    store: set the reference to the store
+    parentCache: set reference to deeper caches
+  ]
+ */ 
+function jsCache(cacheSetSize, setBlockSize, store, parentCache) {
   this.cacheSetSize = cacheSetSize
   this.setBlockSize = setBlockSize
   this.parentCache = parentCache 
   this.store = store  
   this.cacheSets = []
 
-  // create empty arrays to hold data
+  // create empty queues to be sets
   for(var i = 0; i < cacheSetSize; i++){
     this.cacheSets[i] = new Queue
   }
-}
-// write information to cache after finding the set and the block
-jsCache.prototype.writeToCache = function(key, value){
-  // find the set using a hash
-  var cacheSetsIndex = cacheSetHash(key, this.cacheSetSize);
-  // find the position of the data in that set 
-  var cacheBlock = this.cacheSets[cacheSetsIndex]
-  var cacheLine = this.checkCacheSet(key)
-  var parentCache = this.parentCache
-  /*
-    CACHE HIT
-  */
-  if(cacheLine["matchFound"] === true) {
 
-    // update value in cache current cache
-    cacheLine["data"][1] = value
-
-
-  /*
-    CACHE MISS
-  */ 
-
-  } else if (cacheLine["matchFound"] === false && cacheLine["hasRoom"] === true) {
-    // add data to block
-    // if cache-miss and there is no empty spot
-    cacheBlock.enqueue([key, value])
-
-  } else if (cacheLine["matchFound"] === false && cacheLine["hasRoom"] === false){
-
-    // evict last element 
-    cacheBlock.dequeue()
-    // push in new data to the set
-    cacheBlock.enqueue([key, value])
-      // if there is no lower level cache, update the store
-      this.writeToStore(key, value)
-
-  }
-  // update any lower level caches
-  if(parentCache){
-    parentCache.writeToCache(key, value)
-  } 
 
 }
 
-jsCache.prototype.writeToStore = function(key, value) {
-  this.store[key] = value
-}
-
-// check to see if there is room 
-jsCache.prototype.checkCacheSet = function(key){
-
-  var cacheSetsIndex = cacheSetHash(key, this.cacheSetSize);
-  var cacheSetBlock = this.cacheSets[cacheSetsIndex].getState()
-
-  var hasRoom = cacheSetBlock.length < this.setBlockSize ? true : false;
-  var matchFound = false
-  var data;
-
-  
-  
-  for(var i = 0; i < cacheSetBlock.length; i++){
-    // check to see if there is an empty spot in block
-    // TODO add a way to check if really empty of spot is altered
-
-    if(!cacheSetBlock[i].length === 0){
-
-    } else {
-      // check to see if information is in the set
-      if(cacheSetBlock[i][0] === key){
-        matchFound = true
-        data = cacheSetBlock[i]
-      }
-    }
-  }
-
-  // return an object with the status of this set
-  return {
-    hasRoom: hasRoom,
-    matchFound: matchFound,
-    data: data
-  }
-}
-
-
-// retrieve information from assoiative block
+// public. jsCache Method that fetches data from our cache
 jsCache.prototype.fetchData = function(key){
   var cacheLine = this.checkCacheSet(key)
 
@@ -152,24 +92,108 @@ jsCache.prototype.fetchData = function(key){
       return data
     } else {
       // if caches all missed, fetch from store.
-      debugger
+      var data = this.fetchDataFromStore(key)
+      // allocate to the current cache
       this.writeToCache(key, data)
-      return this.fetchDataFromStore(key)
+      return data
     }
 
   }
 
 }
 
+// public. write information to cache after finding the set and the block
+jsCache.prototype.writeToCache = function(key, value){
+  // find the set using a hash
+  var cacheSetsIndex = cacheSetHash(key, this.cacheSetSize);
+  // find the position of the data in that set 
+  var cacheSet = this.cacheSets[cacheSetsIndex]
+  // get an object that holds information about the cache block
+  var cacheBlock = this.checkCacheSet(key)
+  var parentCache = this.parentCache
+  /*
+    CACHE HIT
+  */
+  if(cacheBlock["matchFound"] === true) {
+
+    // update value in cache current cache
+    cacheBlock["data"][1] = value
+
+
+  /*
+    CACHE MISS
+  */ 
+    // if cache-miss and there is room in the block
+  } else if (cacheBlock["matchFound"] === false && cacheBlock["hasRoom"] === true) {
+    // add data to block
+    cacheSet.enqueue([key, value])
+
+    // if cache-miss and there is no empty spot
+  } else if (cacheBlock["matchFound"] === false && cacheBlock["hasRoom"] === false){
+
+    // evict last element 
+    cacheSet.dequeue()
+    // push in new data to the set
+    cacheSet.enqueue([key, value])
+      // if there is no lower level cache, update the store
+      this.writeToStore(key, value)
+
+  }
+  // update any lower level caches
+  if(parentCache){
+    parentCache.writeToCache(key, value)
+  } 
+
+}
+
+// private. jsCache Method to write data to the store 
+jsCache.prototype.writeToStore = function(key, value) {
+  this.store[key] = value
+}
+
+// private. jsCache Method that checks the cache set for cache hit and misses
+// Output: 
+  // hasRoom(Boolean): true if cache has room
+  // matchFound(Boolean): true if requested data was found in cache
+  // data(Queue): a reference to the cache line if available otherwise returns undefined
+// 
+jsCache.prototype.checkCacheSet = function(key){
+  // use the hash function to find the correct set index
+  var cacheSetsIndex = cacheSetHash(key, this.cacheSetSize);
+  // get a reference to the cache line
+  var cacheSetBlock = this.cacheSets[cacheSetsIndex].getState()
+  // compare the block size with the instance of this caches setblock size property
+  var hasRoom = cacheSetBlock.length < this.setBlockSize ? true : false;
+  // default value set to false
+  var matchFound = false
+  var data;
+
+  for(var i = 0; i < cacheSetBlock.length; i++){
+    // check to see if there is an empty spot in block
+    if(!cacheSetBlock[i].length === 0){
+
+    } else {
+      // check to see if information is in the set
+      if(cacheSetBlock[i][0] === key){
+        matchFound = true
+        data = cacheSetBlock[i]
+      }
+    }
+  }
+
+  // return an object with the status of this set
+  return {
+    hasRoom: hasRoom,
+    matchFound: matchFound,
+    data: data
+  }
+}
+
+
+// private. js cache method
 jsCache.prototype.fetchDataFromStore = function(key) {
   return this.store[key]
 }
 
-
-// store is represented as key:value pairs
-// the key represents the address 
-function Store(){
-  this.store = {}
-}
 
 
--- CacheClassSpec.js
+++ CacheClassSpec.js
@@ -1,122 +1,134 @@
 describe("JSCacheClass", function() {
   var cache;
-  var store = {
+  var store; 
 
-  }
   beforeEach(function() {
-    cache2 = new jsCache(6,6, store)
-    cache = new jsCache(3, 3, store, cache2)
+    // before each test create new hash map
+    store = new Store()
+    // before each test create new caches
+    cache2 = new jsCache(6,6, store);
+    cache = new jsCache(3, 3, store, cache2);
     
   })
 
   it('should create a cache instance', function() {
-    expect(cache).to.be.an('object')
+    expect(cache).to.be.an('object');
   })
 
   describe('CacheHits', function() {
     
-    describe('writing data', function() {
+    describe('write(cache hit)', function() {
       it('should write to the cache', function() {
-        cache.writeToCache('abc', 'foobar')
-        var checkCache = cache.checkCacheSet('abc')
-        expect(checkCache["data"]).to.deep.equal(['abc', 'foobar'])
-        expect(checkCache["matchFound"]).to.equal(true)
+        cache.writeToCache('abc', 'foobar');
+        var checkCache = cache.checkCacheSet('abc');
+        expect(checkCache["data"]).to.deep.equal(['abc', 'foobar']);
+        expect(checkCache["matchFound"]).to.equal(true);
       })
 
-      it('should reupdate value in l1 on cache1 hit', function() {
-        cache.writeToCache('abc', 'foobar')
-        cache.writeToCache('abc', 'helloworld')
-        var checkCache = cache.checkCacheSet('abc')
-        expect(checkCache["data"]).to.deep.equal(['abc', 'helloworld'])
-        expect(checkCache["matchFound"]).to.equal(true)
+      it('should reupdate value in l1', function() {
+        cache.writeToCache('abc', 'foobar');
+        cache.writeToCache('abc', 'helloworld');
+        var checkCache = cache.checkCacheSet('abc');
+        expect(checkCache["data"]).to.deep.equal(['abc', 'helloworld']);
+        expect(checkCache["matchFound"]).to.equal(true);
       })
 
       it('should update l2 when l1 cache is updated', function() {
-        cache.writeToCache('123', '321')
-        cache.writeToCache('456', '654')
+        // write data to our cache
+        cache.writeToCache('123', '321');
+        cache.writeToCache('456', '654');
+        // use cache method to check for data
         var checkCache2 = [
           cache2.checkCacheSet('123'),
           cache2.checkCacheSet('456')
-        ] 
-        cache2.checkCacheSet('123')
-        expect(checkCache2[0]["data"]).to.deep.equal(['123', '321'])
-        expect(checkCache2[0]["matchFound"]).to.equal(true)
-        expect(checkCache2[1]["data"]).to.deep.equal(['456', '654'])
-        expect(checkCache2[1]["matchFound"]).to.equal(true)
+        ]; 
+        // writing to cache1 should update cache2        
+        expect(checkCache2[0]["data"]).to.deep.equal(['123', '321']);
+        expect(checkCache2[0]["matchFound"]).to.equal(true);
+        expect(checkCache2[1]["data"]).to.deep.equal(['456', '654']);
+        expect(checkCache2[1]["matchFound"]).to.equal(true);
 
         // should not update store
-        expect(store['123']).to.equal(undefined)
-        expect(store['456']).to.equal(undefined)
+        expect(store['123']).to.equal(undefined);
+        expect(store['456']).to.equal(undefined);
       })
     })
     
-    describe('fetching data', function() {
+    describe('fetch(cache hit)', function() {
       it('should fetch data from the cache', function() {
-        cache.writeToCache('abc', 'foobar1')
-        expect(cache.fetchData('abc')).to.equal('foobar1')        
+        cache.writeToCache('abc', 'foobar1');
+        expect(cache.fetchData('abc')).to.equal('foobar1');
       })
     })
   })
 
   describe('CacheMisses', function() {
-    describe('write back', function() {
-      it('should evict data from the block when full and update the store with the evicted data', function() {
+    describe('write(cache miss)', function() {
+      it('should evict data from the block when full and update the store with the evicted data (writeback)', function() {
         // add data to cache
-        cache.writeToCache('1', 'foobar')
-        cache.writeToCache('2', 'foobar')
-        cache.writeToCache('3', 'foobar')
-        cache.writeToCache('4', 'helloworld')
+        cache.writeToCache('1', 'foobar');
+        cache.writeToCache('2', 'foobar');
+        cache.writeToCache('3', 'foobar');
+        cache.writeToCache('4', 'helloworld');
 
-        var checkCacheKey1 = cache.checkCacheSet('1')
-        var checkCacheKey4 = cache.checkCacheSet('4')
+        var checkCacheKey1 = cache.checkCacheSet('1');
+        var checkCacheKey4 = cache.checkCacheSet('4');
 
         // adding 4 should evict the first item in queue
         // 1 [2,3,4]
-        expect(store['4']).to.equal('helloworld')
+        expect(store['4']).to.equal('helloworld');
         // after evicting should not be in cache
-        expect(checkCacheKey1['matchFound']).to.equal(false)
-        expect(checkCacheKey1['data']).to.equal(undefined)
-      })
-      it('should evict data from l2 cache when l2 cache is full', function() {
-
+        expect(checkCacheKey1['matchFound']).to.equal(false);
+        expect(checkCacheKey1['data']).to.equal(undefined);
       })
       
+      it('should write to l2 and then allocate to l1 if cache miss on l1', function() {
+        // write only to l2
+        cache2.writeToCache('1a', 'a1');
+        // simulate a cache miss by fetching from data that l1 doesnt have but l2 does
+        cache.fetchData('1a');
+        // get the state of the set
+        var checkCache1 = cache.checkCacheSet('1a');
+        // check to see if the data is in the l1 cache
+        expect(checkCache1['data']).to.deep.equal(['1a','a1']);
+      })      
 
     })
-    describe('fetching data', function() {
-      // cache miss
+    describe('fetch(cache miss)', function() {
+    
       it('should fetch from store if not in cache', function() {
-        store['cde'] = 'edc123'
-        expect(cache.fetchData('cde')).to.equal('edc123')        
+        // put item into store
+        store['cde'] = 'edc123';
+        // cache is empty should get the information from the cache
+        expect(cache.fetchData('cde')).to.equal('edc123');
       })
-      it('should fetch from parent cache if not in current cache', function() {
-        cache2.writeToCache('def', '123fed')
-        expect(cache.fetchData('def')).to.equal('123fed')        
-      })
-      it('should write to l2 and then allocate to l1 if cache miss on l1 but not l2', function() {
-        // write only to l2
-        cache2.writeToCache('1a', 'a1')
-        // simulate a cache miss by fetching from data that l1 doesnt have but l2 does
-        cache.fetchData('1a')
-        // get the state of the set
-        var checkCache1 = cache.checkCacheSet('1a')
-        // check to see if the data is in the l1 cache
-        expect(checkCache1['data']).to.deep.equal(['1a','a1'])
-      })      
-      it('should allocate to l2 and l1 if fetched from store', function() {
-        store['c3'] = '3c'
-        var checkCache1 = cache.checkCacheSet('c3')
-        var checkCache2 = cache2.checkCacheSet('c3')
 
-        expect(checkCache1['data']).to.equal(undefined)
-        expect(checkCache2['data']).to.equal(undefined)
-        cache2.fetchData('c3')
+      it('should fetch from parent cache if not in current cache', function() {
+        // write data only to l2 cache
+        cache2.writeToCache('def', '123fed');
+        // request to l1 cache
+        expect(cache.fetchData('def')).to.equal('123fed');
+      })
+      it('should allocate to l2 and l1 if fetched from store', function() {
+        // set value in store
+        store['c3'] = '3c';
+
+        // initial state of our cache should be empty
+        var checkCache1 = cache.checkCacheSet('c3');
+        var checkCache2 = cache2.checkCacheSet('c3');
+        expect(checkCache1['data']).to.equal(undefined);
+        expect(checkCache2['data']).to.equal(undefined);
+
+        // simulate a l1 and l2 cache miss
+        cache.fetchData('c3');
         
-        checkCache1 = cache.checkCacheSet('c3')
-        checkCache2 = cache.checkCacheSet('c3')
-        // var checkCache2 = cache2.checkCacheSet('c3')
-        console.log(checkCache2)
-        expect(checkCache2['data']).to.deep.equal(['c3', '3c'])
+        // recheck the state of our caches
+        checkCache1 = cache.checkCacheSet('c3');
+        checkCache2 = cache2.checkCacheSet('c3');
+        
+        // data should be allocated to l1 and l2 cache after cache
+        expect(checkCache2['data']).to.deep.equal(['c3', '3c']);
+        expect(checkCache1['data']).to.deep.equal(['c3', '3c']);
 
       })
     })
